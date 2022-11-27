e044e4e65fee9e87b5e27f86856f6d38fd2aba8b
Malik Mubashir <malikmubashir025@gmail.com>
2022-11-27 19:29:27 +0500

    if this commit gets merged it will fix contact us section

--- src/pages/home/Contact.js
+++ src/pages/home/Contact.js
@@ -2,96 +2,97 @@ import { Box, TextField, Button } from "@mui/material";
 import React from "react";
 function Contact() {
   return (
-    <Box className="flex flex-col w-full h-full mt-16 p-20">
-      <span className="font-bold text-[54px] text-center w-full p-[1.4rem] sm:text-[1.5rem] sm:p-[10px]">
+    <Box className='flex flex-col w-full h-full mt-16 p-20'>
+      <span className='font-bold text-[54px] text-center w-full p-[1.4rem] sm:text-[1.5rem] sm:p-[10px]'>
         Contact Us
       </span>
-      <Box className="grid contactUs mt-10 sm:w-full">
-        <Box className="w-full h-full grid sm:place-self-center">
-          <img className="sm:hidden" src="/assets/logoBlack.svg" />
+      <Box className='grid contactUs sm:w-full p-40'>
+        {/* ml-32 */}
+        <Box className='w-full h-full grid sm:place-self-center '>
+          <img className='sm:hidden p-9' src='/assets/logoBlack.svg' />
 
-          <Box className="grid grid-flow-col grid-auto-2 gap-x-10  ml-8 text-[1.3rem] md:text-[1rem] sm:justify-center sm:mt-[20px] ">
+          <Box className='grid grid-flow-col grid-auto-2 ml-8 text-[1.3rem] md:text-[1rem] sm:justify-center sm:mt-[20px] '>
             <img
-              src="/assets/location.svg"
-              className="row-start-1 col-start-1"
+              src='/assets/location.svg'
+              className='row-start-1 col-start-1 p-9'
             />
-            <Box className="row-start-1 col-start-2 self-center ">
+            <Box className='row-start-1 col-start-2 self-center p-9'>
               1980 lorem road usa
             </Box>
             <img
-              src="/assets/message.svg"
-              className="row-start-2 col-start-1 self-center"
+              src='/assets/message.svg'
+              className='row-start-2 col-start-1 self-center p-9'
             />
-            <Box className="row-start-2 col-start-2 self-center">
+            <Box className='row-start-2 col-start-2 self-center p-9'>
               vdm@email.com
             </Box>
             <img
-              src="/assets/phone.svg"
-              className="row-start-3 col-start-1 self-center"
+              src='/assets/phone.svg'
+              className='row-start-3 col-start-1 self-center p-9'
             />
-            <Box className="row-start-3 col-start-2 self-center">
+            <Box className='row-start-3 col-start-2 self-center p-9'>
               259-634-756
             </Box>
 
-            <Box className="row-start-4 w-full py-[1.5rem] flex flex-row gap-5 col-span-2 ">
-              <img src="/assets/instagram.svg" />
-              <img src="/assets/twitter.svg" />
-              <img src="/assets/facebook.svg" />
+            <Box className='row-start-4 w-full py-[1.5rem] flex flex-row gap-14 col-span-2 p-9'>
+              <img src='/assets/instagram.svg' />
+              <img src='/assets/twitter.svg' />
+              <img src='/assets/facebook.svg' />
             </Box>
           </Box>
         </Box>
         {/* Form Below */}
-        <Box className=" w-full h-full flex flex-col">
-          <Box className="w-full h-full grid grid-cols-2 gap-10">
+        <Box className=' w-full h-full flex flex-col'>
+          <Box className='w-full h-full grid grid-cols-2 gap-20'>
             <TextField
-              id="standard-number"
-              label="First Name"
+              id='standard-number'
+              label='First Name'
               InputLabelProps={{
                 shrink: true,
               }}
-              variant="standard"
+              variant='standard'
             />
             <TextField
-              id="standard-number"
-              label="Last Name"
+              id='standard-number'
+              label='Last Name'
               InputLabelProps={{
                 shrink: true,
               }}
-              variant="standard"
+              variant='standard'
             />
             <TextField
-              id="standard-number"
-              label="Email Address"
+              id='standard-number'
+              label='Email Address'
               InputLabelProps={{
                 shrink: true,
               }}
-              variant="standard"
+              variant='standard'
             />
             <TextField
-              id="standard-number"
-              label="Phone Number"
+              id='standard-number'
+              label='Phone Number'
               InputLabelProps={{
                 shrink: true,
               }}
-              variant="standard"
+              variant='standard'
             />
             <TextField
-              id="standard-number"
-              label="Message"
+              id='standard-number'
+              label='Message'
               InputLabelProps={{
                 shrink: true,
               }}
-              variant="standard"
-              className="col-span-2"
+              variant='standard'
+              className='col-span-2'
             />
           </Box>
           <Button
-            variant="contained"
-            className="registerButton  w-fit h-fit sm:place-self-center "
+            variant='contained'
+            className='registerButton  w-fit h-fit sm:place-self-center '
             style={{ marginTop: "2rem" }}
-            color="secondary"
+            color='secondary'
           >
-            <span className="px-6">Submit</span>
+            <span className='px-6'>Submit</span>
           </Button>
         </Box>
       </Box>
